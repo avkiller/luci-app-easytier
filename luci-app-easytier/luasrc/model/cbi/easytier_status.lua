a1c942d1e6ef2035e9254f62736aba806fc5912a
fireworld <fuxudong103@gmail.com>
2026-06-19 02:55:21 +0800

    修改部分内容

--- luci-app-easytier/luasrc/model/cbi/easytier_status.lua
+++ luci-app-easytier/luasrc/model/cbi/easytier_status.lua
@@ -3,9 +3,9 @@ local http = luci.http
 m = Map("easytier", translate("EasyTier Status"))
 m.description = translate("A simple, secure, decentralized VPN solution for intranet penetration, implemented in Rust using the Tokio framework. "
         .. "Project URL: <a href=\"https://github.com/avkiller/EasyTier\" target=\"_blank\">github.com/avkiller/EasyTier</a>&nbsp;&nbsp;")
-        .. "<a href=\"https://github.com/avkiller/EasyTier/releases\" target=\"_blank\"><img alt=\"\" src=\"https://img.shields.io/github/v/tag/EasyTier/EasyTier?include_prereleases&logo=github&label=tag&link=https%3A%2F%2Fgithub.com%2FEasyTier%2FEasyTier%2Freleases\" style=\"vertical-align:middle;\"></a>&nbsp;"
-        .. "<a href=\"https://github.com/avkiller/EasyTier/releases\" target=\"_blank\"><img alt=\"\" src=\"https://img.shields.io/github/v/release/EasyTier/EasyTier?logo=github&link=https%3A%2F%2Fgithub.com%2FEasyTier%2FEasyTier%2Freleases\" style=\"vertical-align:middle;\"></a>&nbsp;"
-        .. "<a href=\"https://github.com/avkiller/luci-app-easytier/releases\" target=\"_blank\"><img alt=\"\" src=\"https://img.shields.io/github/v/release/EasyTier/luci-app-easytier?logo=openwrt&label=luci&link=https%3A%2F%2Fgithub.com%2FEasyTier%2Fluci-app-easytier%2Freleases\" style=\"vertical-align:middle;\"></a>"
+        .. "<a href=\"https://github.com/avkiller/EasyTier/releases\" target=\"_blank\"><img alt=\"\" src=\"https://img.shields.io/github/v/tag/avkiller/EasyTier?include_prereleases&logo=github&label=tag&link=https%3A%2F%2Fgithub.com%2Favkiller%2FEasyTier%2Freleases\" style=\"vertical-align:middle;\"></a>&nbsp;"
+        .. "<a href=\"https://github.com/avkiller/EasyTier/releases\" target=\"_blank\"><img alt=\"\" src=\"https://img.shields.io/github/v/release/avkiller/EasyTier?logo=github&link=https%3A%2F%2Fgithub.com%2Favkiller%2FEasyTier%2Freleases\" style=\"vertical-align:middle;\"></a>&nbsp;"
+        .. "<a href=\"https://github.com/avkiller/luci-app-easytier/releases\" target=\"_blank\"><img alt=\"\" src=\"https://img.shields.io/github/v/release/avkiller/luci-app-easytier?logo=openwrt&label=luci&link=https%3A%2F%2Fgithub.com%2FEasyTier%2Fluci-app-easytier%2Freleases\" style=\"vertical-align:middle;\"></a>"
 m.pageaction = false
 
 -- 状态卡片
